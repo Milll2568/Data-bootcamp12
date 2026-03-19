1da9638135658d04baa8fafcd8d8dd6375539f22
Milll2568 <szymi075555@gmail.com>
2026-03-19 04:07:17 +0000

    add add_two function

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 # print("Hello World")
+import pprint
+from ssl import ALERT_DESCRIPTION_BAD_CERTIFICATE
 print("Hello World")
 
 # print good bye
@@ -6,3 +8,9 @@ print("good bye")
 
 # print new line
 print("new line")
+
+# new function
+def add_two(num):
+    return num + 2
+
+print(add_two(2), add_two(5))
